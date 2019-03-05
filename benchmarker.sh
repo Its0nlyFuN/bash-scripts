ba80a9dc0a38e8bacdc8429ab0ac102ffce343c0
Richard Gladman <gitlab@pything.dev>
2019-03-05 12:50:31 +0000

    Make use of $TMPDIR consistent and remove unneccessary cd commands. Fix but in stats generation

--- benchmarker.sh
+++ benchmarker.sh
@@ -8,6 +8,7 @@ TMPDIR="$1"
 LOGFILE="$TMPDIR/benchie_${CDATE}.log"
 RAMSIZE=`awk '/MemAvailable/{print $2}' /proc/meminfo`
 DEPS="$(pacman -Qkq {perf,unzip,darktable,sysbench,nasm,time,make} 2>/dev/null; echo $?)"
+TESTS=6
 
 if [[ -z $1 ]] ; then
 	echo "Please specify the full path for the temporary directory! Aborting."
@@ -50,51 +51,44 @@ echo "======      torvic9     ======"
 echo "------------------------------"
 
 runffm() {
-	cd $TMPDIR
-	tar xf ffmpeg.tar.bz2
-	cd ffmpeg-4.1
+    tar xf $TMPDIR/ffmpeg.tar.bz2 -C $TMPDIR/
+	cd $TMPDIR/ffmpeg-4.1
 	./configure --quiet --disable-debug --enable-static --enable-gpl --disable-nvdec --disable-nvenc --disable-ffnvcodec --disable-vaapi --disable-vdpau --disable-doc --disable-appkit --disable-avfoundation --disable-sndio --disable-schannel --disable-securetransport --disable-amf --disable-cuvid  --disable-d3d11va --disable-dxva2
 	local START=`/usr/bin/time -f %e -o $TMPDIR/runffm make -s -j$(nproc) &>/dev/null`
-	cd .. && rm -rf ffmpeg-4.1/
+	cd ../..
+	rm -rf $TMPDIR/ffmpeg-4.1/
 }
 
 runxz() {
-	cd $TMPDIR
- 	unzip silesia.zip -d silesia/
- 	tar cf silesia.tar silesia/
- 	rm -rf silesia/
+ 	unzip $TMPDIR/silesia.zip -d $TMPDIR/silesia/
+ 	tar cf $TMPDIR/silesia.tar $TMPDIR/silesia/
+ 	rm -rf $TMPDIR/silesia/
  	local START=`/usr/bin/time -f %e -o $TMPDIR/runxz xz -z -T$(nproc) -7 -Qq $TMPDIR/silesia.tar`
-	rm *.xz
+	rm $TMPDIR/*.xz
 }
 
 runperf() {
-	cd $TMPDIR
 	local START=`perf bench -f simple sched messaging -p -t -g 25 -l 10000`
 	echo "$START"
 }
 
 runpi() {
-	cd $TMPDIR
 	local START=`/usr/bin/time -f%e -o $TMPDIR/runpi bc -l -q <<< "scale=6666; 4*a(1)" 1>/dev/null` 
 }
 
 rundarkt() {
-	cd $TMPDIR
  	local START=`darktable-cli $TMPDIR/bench.srw benchtest_$CDATE.jpg --core --disable-opencl -d perf 2>/dev/null | awk '/dev_process_export/{print $1}'`
  	echo "$START"
 }
 
 runsysb1() {
-	cd $TMPDIR
  	local START=`/usr/bin/time -f %e -o $TMPDIR/runsysb1 sysbench --threads=$(nproc) --verbosity=0 --events=30000 --time=0 cpu run --cpu-max-prime=30000`
 }
 runsysb2() {
-	cd $TMPDIR
  	local START=`/usr/bin/time -f %e -o $TMPDIR/runsysb2 sysbench --threads=$(nproc) --verbosity=0 --time=0 memory run --memory-total-size=64G --memory-block-size=4K --memory-access-mode=rnd`
 }
 
 echo "Running..."
-cd $TMPDIR
 START="$(runperf)" ; sleep 2
 echo "Perf sched: $START" > $LOGFILE
 START="$(runpi)" ; sleep 2
@@ -123,8 +117,9 @@ arrayz=(`awk -F': ' '{print $2}' $LOGFILE`)
 echo "------------------------------"
 cat $LOGFILE
 
+ARRAY=()
 let i=0
-for ((i=0 ; i<=6 ; i++)) ; do
+for ((i=0 ; i<$TESTS ; i++)) ; do
 	ARRAY[$i]="$(echo "scale=3; sqrt(${arrayz[$i]}*12)" | bc -l)"
 done
 
