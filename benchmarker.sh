55aab5acaee2a442f37183ff4936faea83e67082
torvic9 <torvic9@mailbox.org>
2019-12-16 11:51:21 +0100

    ffmpeg replaces x265, algo changes

--- benchmarker.sh
+++ benchmarker.sh
@@ -3,20 +3,27 @@
 # Contributors and testers:
 # Richard Gladman, William Pursell, SGS, mbb, mbod, Manjaro Forum, StackOverflow
 
-runx265() {
-	#if [[ -d $WORKDIR/x265 ]] ; then rm -rf $WORKDIR/x265 ; fi
-	cd $WORKDIR/x265
-	mkdir build-10 && cd build-10
-	local RESFILE="$WORKDIR/runx265"
-	cmake ../source -DCMAKE_INSTALL_PREFIX=/tmp -DENABLE_SHARED=FALSE -DHIGH_BIT_DEPTH=TRUE \
-		-DENABLE_HDR10_PLUS=TRUE -DNATIVE_BUILD=TRUE &>/dev/null
+runffm() {
+	cd $WORKDIR
+	tar xf ffmpeg.tar.gz
+	cd ffmpeg-1529dfb
+	local RESFILE="$WORKDIR/runffm"
+	./configure --prefix=/tmp --disable-debug --enable-static --enable-stripping \
+	  --disable-ladspa --disable-programs --disable-ffplay --disable-ffprobe \
+	  --disable-doc --disable-network --disable-protocols --disable-lzma \
+	  --disable-amf --disable-cuda-llvm --disable-cuvid --disable-d3d11va --disable-dxva2 \
+	  --disable-nvdec --disable-nvenc --disable-vaapi --disable-vdpau --disable-sdl2 \
+	  --disable-schannel --disable-securetransport --enable-libfontconfig \
+	  --enable-libfreetype --enable-libspeex --enable-libvpx --enable-libopus --enable-libvorbis \
+	  --enable-libx264 --enable-libx265 --enable-opengl --enable-libdrm --enable-gpl \
+	  --enable-gmp --enable-gnutls --disable-avx512 --disable-fma4 --disable-autodetect \
+	  --enable-version3 &>/dev/null
 	/usr/bin/time -f %e -o $RESFILE make -s -j${CPUCORES} &>/dev/null &
 	local PID=$!
-	echo -n -e "* x265 compilation:\t\t\t"
+	echo -n -e "\n* ffmpeg compilation:\t\t\t"
 	local s='-\|/'; local i=0; while kill -0 $PID &>/dev/null ; do i=$(( (i+1) %4 )); printf "\b${s:$i:1}"; sleep .5; done
 	printf "\b " ; cat $RESFILE
-	echo "x265 compilation: $(cat $RESFILE)" >> $LOGFILE
-	cd .. && rm -rf build-10
+	echo "ffmpeg compilation: $(cat $RESFILE)" >> $LOGFILE
 	return 0
 }
 
@@ -48,7 +55,7 @@ runblend() {
 
 runargon() {
 	local RESFILE="$WORKDIR/runargon"
-	/usr/bin/time -f %e -o $RESFILE argon2 BenchieSalt -i -t 50 -m 20 -p $CPUCORES &>/dev/null <<< $(dd if=/dev/urandom bs=1 count=64 status=none) &
+	/usr/bin/time -f %e -o $RESFILE argon2 BenchieSalt -i -t 60 -m 20 -p $CPUCORES &>/dev/null <<< $(dd if=/dev/urandom bs=1 count=64 status=none) &
 	local PID=$!
 	echo -n -e "* Argon2 hashing:\t\t\t"
 	local s='-\|/'; local i=0; while kill -0 $PID &>/dev/null ; do i=$(( (i+1) %4 )); printf "\b${s:$i:1}"; sleep .5; done
@@ -95,8 +102,8 @@ rundarkt() {
 
 runsysb1() {
 	local RESFILE="$WORKDIR/runsysb1"
- 	/usr/bin/time -f %e -o $RESFILE sysbench --threads=$CPUCORES --verbosity=0 --events=20000 \
- 	--time=0 cpu run --cpu-max-prime=60000 &
+ 	/usr/bin/time -f %e -o $RESFILE sysbench --threads=$CPUCORES --verbosity=0 --events=10000 \
+ 	--time=0 cpu run --cpu-max-prime=80000 &
 	local PID=$!
 	echo -n -e "* Sysbench CPU:\t\t\t\t"
 	local s='-\|/'; local i=0; while kill -0 $PID &>/dev/null ; do i=$(( (i+1) %4 )); printf "\b${s:$i:1}"; sleep .5; done
@@ -108,7 +115,7 @@ runsysb1() {
 runsysb2() {
 	local RESFILE="$WORKDIR/runsysb2"
  	/usr/bin/time -f %e -o $RESFILE sysbench --threads=$CPUCORES --verbosity=0 --time=0 \
- 	memory run --memory-total-size=100G --memory-block-size=2M --memory-oper=write --memory-access-mode=rnd &>/dev/null &
+ 	memory run --memory-total-size=100G --memory-block-size=512K --memory-oper=write --memory-access-mode=rnd &>/dev/null &
 	local PID=$!
 	echo -n -e "* Sysbench RAM write:\t\t\t"
 	local s='-\|/'; local i=0; while kill -0 $PID &>/dev/null ; do i=$(( (i+1) %4 )); printf "\b${s:$i:1}"; sleep .5; done
@@ -137,7 +144,7 @@ killproc() {
 
 exitproc() {
 	echo -e "Removing temporary files...\n"
-	for i in $WORKDIR/{run*,benchie_*.jpg,kernel49.tar.xz,blender*.png,darktablerc,data.db,blender,pi} ; do
+	for i in $WORKDIR/{run*,benchie_*.jpg,kernel44.tar.xz,ffmpeg-1529dfb,blender*.png,darktablerc,data.db*,blender,pi} ; do
 		if [ -f $i ] ; then rm $i ; fi
 		if [ -d $i ] ; then rm -r $i ; fi
 	done
@@ -155,6 +162,7 @@ CPUCORES=`nproc`
 #fi
 CPUMHZ=$(lscpu -e=maxmhz | tail -n1)
 CPUGHZ=$(echo "scale=1; ${CPUMHZ%%,*} / 1000" | bc)
+COEFF=$(echo "scale=2; l(${CPUCORES} / 2 + ${CPUGHZ})" | bc -l)
 NRTESTS=10
 SYSINFO=$(inxi -c0 -v | sed "s/Up:.*//;s/inxi:.*//;s/Storage:.*//")
 
@@ -191,9 +199,8 @@ if [[ ! -f $WORKDIR/bench.srw && ! -f $WORKDIR/bench.srw.xmp ]]; then
  	wget --show-progress -qO $WORKDIR/bench.srw http://www.mirada.ch/bench.SRW
  	wget --show-progress -qO $WORKDIR/bench.srw.xmp http://www.mirada.ch/bench.SRW.xmp
 fi
-if [[ ! -d $WORKDIR/x265 ]]; then
-	echo "cloning x265 repo ..."
-	hg clone http://hg.videolan.org/x265 $WORKDIR/x265 -u 3.2 -q
+if [[ ! -f $WORKDIR/ffmpeg.tar.gz ]]; then
+	wget --show-progress -qO $WORKDIR/ffmpeg.tar.gz https://git.ffmpeg.org/gitweb/ffmpeg.git/snapshot/1529dfb73a5157dcb8762051ec4c8d8341762478.tar.gz
 fi
 if [[ ! -f $WORKDIR/blender.zip ]]; then
 	wget --show-progress -qO $WORKDIR/blender.zip https://download.blender.org/demo/test/Demo_274.zip
@@ -219,8 +226,8 @@ runargon ; sleep 2
 runsysb1 ; sleep 2
 runsysb2 ; sleep 2
 runsysb3 ; sleep 2
+runffm ; sync ; sleep 2
 rundarkt ; sync ; sleep 2
-runx265 ; sync ; sleep 2
 runxz ; sync ; sleep 2
 runblend ; sync ; sleep 2
 
@@ -229,20 +236,20 @@ arrayz=(`awk -F': ' '{print $2}' $LOGFILE`)
 # arrayn not used currently
 # arrayn=(`awk -F': ' '{print $1}' $LOGFILE`)
 # watch!
-set -x
+#set -x
 # watch!
 
 for ((i=0 ; i<$(( $NRTESTS - 4)) ; i++)) ; do
-	ARRAY[$i]="$(echo "scale=10; ${arrayz[$i]} * sqrt(${arrayz[$i]} * 0.3) * l($CPUCORES / 2 + $CPUGHZ)" | bc -l)"
+	ARRAY[$i]="$(echo "scale=10; (${arrayz[$i]} * 0.1) * sqrt(${arrayz[$i]})" | bc -l)"
 done
 for ((i=$(( $NRTESTS - 4 )) ; i<$NRTESTS ; i++)) ; do
-	ARRAY[$i]="$(echo "scale=10; ${arrayz[$i]} * sqrt(${arrayz[$i]} * 0.4) * l($CPUCORES / 2 + $CPUGHZ)" | bc -l)"
+	ARRAY[$i]="$(echo "scale=10; (${arrayz[$i]} * 0.2) * sqrt(${arrayz[$i]})" | bc -l)"
 done
 
 #TTIME="$(echo "${arrayz[@]}" | sed 's/ /+/g' | bc)"
 TTIME="$(IFS="+" ; bc <<< "scale=2; ${arrayz[*]}")"
-INTSCORE="$(IFS="+" ; bc <<< "scale=10; ${ARRAY[*]}")"
-SCORE="$(bc -l <<< "scale=2; 1000 - ($INTSCORE / $NRTESTS)")"
+INTSCORE="$(IFS="+" ; bc -l <<< "scale=2; ${ARRAY[*]}")"
+SCORE="$(bc -l <<< "scale=2; $INTSCORE * $COEFF / $NRTESTS")"
 echo "--------------------------------------------------"
 echo "Total time in seconds:"
 echo "--------------------------------------------------"
