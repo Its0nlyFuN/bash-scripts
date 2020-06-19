e8a85ab16d534a25d028dd625ed040945aa830c3
torvic9 <torvic9@mailbox.org>
2020-06-19 21:30:33 +0200

    update benchmarker

--- build-llvm-pgo.sh
+++ build-llvm-pgo.sh
@@ -11,7 +11,7 @@ PKGVER="10.0.1rc1"
 NCORES=`nproc`
 export CFLAGS="-O3 -march=native -pipe"
 export CXXFLAGS="-O3 -march=native -pipe"
-#export LDFLAGS="--as-needed,-z,relro,-z,now"
+#export LDFLAGS="-Wl,-O1"
 COMMONFLAGS="-DLLVM_ENABLE_PROJECTS=clang;clang-tools-extra;compiler-rt;lld \
 -DLLVM_LINK_LLVM_DYLIB=ON \
 -DLLVM_TARGETS_TO_BUILD=X86;AMDGPU
@@ -24,11 +24,11 @@ COMMONFLAGS="-DLLVM_ENABLE_PROJECTS=clang;clang-tools-extra;compiler-rt;lld \
 -DLLVM_BINUTILS_INCDIR=/usr/include \
 -DLLVM_PARALLEL_COMPILE_JOBS=${NCORES} -DLLVM_PARALLEL_LINK_JOBS=${NCORES} \
 -DLLVM_ENABLE_BINDINGS=OFF \
+-DLLVM_ENABLE_OCAMLDOC=OFF \
 -DLLVM_ENABLE_PLUGINS=ON \
 -DLLVM_ENABLE_TERMINFO=OFF \
 -DLLVM_INCLUDE_DOCS=OFF \
 -DLLVM_INCLUDE_EXAMPLES=OFF \
- DLLVM_ENABLE_FFI=ON \
 -DCOMPILER_RT_BUILD_LIBFUZZER=OFF \
 -DCLANG_LINK_CLANG_DYLIB=ON \
 -DCLANG_PLUGIN_SUPPORT=OFF \
@@ -44,12 +44,12 @@ COMMONFLAGS="-DLLVM_ENABLE_PROJECTS=clang;clang-tools-extra;compiler-rt;lld \
 #-DLIBCXX_INCLUDE_BENCHMARKS=OFF"
 #-DLIBCXXABI_LIBCXX_PATH=/usr/lib \
 #-DLIBCXXABI_LIBCXX_INCLUDES=/usr/include \
-
 #-DLIBCXX_INSTALL_PREFIX="$TOPLEV/stage1/install" (add this to each stage)
 
+[[ ! -d $TOPLEV ]] && mkdir $TOPLEV
 cd $TOPLEV
 if [[ ! -d llvm-${PKGVER}.src ]] ; then
-	wget -nc -qO llvm-${PKGVER}.tar.xz --show-progress https://github.com/llvm/llvm-project/releases/download/llvmorg-10.0.1-rc1/llvm-project-${PKGVER}.src.tar.xz
+	wget -nc -qO llvm-${PKGVER}.tar.xz --show-progress https://github.com/llvm/llvm-project/releases/download/llvmorg-10.0.1-rc1/llvm-project-${PKGVER}.tar.xz
 	tar xf llvm-${PKGVER}.tar.xz
 fi
 
@@ -57,13 +57,15 @@ if [[ ! -d $TOPLEV/stage1 ]] ; then
 	mkdir $TOPLEV/stage1
 fi
 cd $TOPLEV/stage1
-ninja clean
-cmake -G Ninja "$TOPLEV/llvm-project-${PKGVER}/llvm" -DCMAKE_C_COMPILER=/usr/bin/gcc -DCMAKE_CXX_COMPILER=/usr/bin/g++ \
+if [[ ! -f ./bin/clang-10 ]] ; then
+	ninja clean
+	cmake -G Ninja "$TOPLEV/llvm-project-${PKGVER}/llvm" -DCMAKE_C_COMPILER=/usr/bin/gcc -DCMAKE_CXX_COMPILER=/usr/bin/g++ \
 -DLLVM_CCACHE_BUILD=ON -DCMAKE_INSTALL_PREFIX="$TOPLEV/stage1/install" \
 -DCOMPILER_RT_BUILD_SANITIZERS=OFF -DLLVM_ENABLE_BACKTRACES=OFF -DLLVM_INCLUDE_TESTS=OFF \
 -DLLVM_INCLUDE_UTILS=OFF ${COMMONFLAGS} || exit 8
-echo "---> STAGE 1"
-ninja install || exit 16
+	echo "----> STAGE 1"
+	ninja install || exit 16
+fi
 
 sleep 2
 
@@ -75,8 +77,8 @@ CPATH=$TOPLEV/stage1/install/bin/
 ninja clean
 cmake -G Ninja "$TOPLEV/llvm-project-${PKGVER}/llvm" -DCMAKE_C_COMPILER=$CPATH/clang \
 -DCMAKE_CXX_COMPILER=$CPATH/clang++ -DCMAKE_INSTALL_PREFIX="$TOPLEV/stage2-gen/install" \
--DLLVM_USE_LINKER=lld -DLLVM_BUILD_INSTRUMENTED=ON ${COMMONFLAGS} || exit 8
-echo "---> STAGE 2 GENERATION"
+-DLLVM_USE_LINKER=lld -DLLVM_BUILD_INSTRUMENTED=IR -DLLVM_BUILD_RUNTIME=OFF ${COMMONFLAGS} || exit 8
+echo "----> STAGE 2 GENERATION"
 ninja install || exit 16
 
 sleep 2
@@ -90,7 +92,7 @@ ninja clean
 cmake -G Ninja "$TOPLEV/llvm-project-${PKGVER}/llvm" -DCMAKE_C_COMPILER=$CPATH/clang \
 -DCMAKE_CXX_COMPILER=$CPATH/clang++ -DCMAKE_INSTALL_PREFIX="$TOPLEV/stage3-train/install" \
 -DLLVM_USE_LINKER=lld ${COMMONFLAGS} || exit 8
-echo "---> STAGE 3 TRAIN"
+echo "----> STAGE 3 TRAIN"
 ninja clang || exit 16
 
 sleep 2
@@ -106,12 +108,12 @@ CPATH=$TOPLEV/stage1/install/bin/
 ninja clean
 cmake -G Ninja "$TOPLEV/llvm-project-${PKGVER}/llvm" -DCMAKE_C_COMPILER=$CPATH/clang \
 -DCMAKE_CXX_COMPILER=$CPATH/clang++ -DCMAKE_INSTALL_PREFIX="$TOPLEV/stage4-final/Release" \
--DLLVM_USE_LINKER=lld -DLLVM_ENABLE_LTO=Thin
+-DLLVM_USE_LINKER=lld -DLLVM_ENABLE_LTO=Full -DLLVM_ENABLE_FFI=ON \
 -DLLVM_PROFDATA_FILE="${TOPLEV}/stage2-gen/profiles/clang.profdata" ${COMMONFLAGS} || exit 8
 echo "---> STAGE 4 FINAL"
 ninja check-lld || exit 32 ; ninja check-clang || exit 32
 ninja install || exit 16
 
-echo "---> DONE!"
-echo "---> clang is in $TOPLEV/stage4-final/Release"
+echo "----> DONE!"
+echo "----> clang is in $TOPLEV/stage4-final/Release"
 
--- mini-benchmarker/benchmarker.sh
+++ mini-benchmarker/benchmarker.sh
@@ -8,7 +8,7 @@ runstress1() {
 	/usr/bin/time -f %e -o $RESFILE $STRESS -q $WORKDIR/stressC &>/dev/null &
 	local PID=$!
 	echo -n -e "* stress-ng cpu:\t\t\t"
-	local s='-\|/'; local i=0; while kill -0 $PID &>/dev/null ; do i=$(( (i+1) %4 )); printf "\b${s:$i:1}"; sleep 1; done
+	local s='-+'; local i=0; while kill -0 $PID &>/dev/null ; do i=$(( (i+1) %2 )); printf "\b${s:$i:1}"; sleep 1; done
 	printf "\b " ; cat $RESFILE
 	echo "stress-ng cpu: $(cat $RESFILE)" >> $LOGFILE
 	return 0
@@ -19,7 +19,7 @@ runstress2() {
 	/usr/bin/time -f %e -o $RESFILE $STRESS -q $WORKDIR/stressR &>/dev/null &
 	local PID=$!
 	echo -n -e "* stress-ng memory:\t\t\t"
-	local s='-\|/'; local i=0; while kill -0 $PID &>/dev/null ; do i=$(( (i+1) %4 )); printf "\b${s:$i:1}"; sleep 1; done
+	local s='-+'; local i=0; while kill -0 $PID &>/dev/null ; do i=$(( (i+1) %2 )); printf "\b${s:$i:1}"; sleep 1; done
 	printf "\b " ; cat $RESFILE
 	echo "stress-ng memory: $(cat $RESFILE)" >> $LOGFILE
 	return 0
@@ -32,7 +32,7 @@ runblend() {
 	/usr/bin/time -f %e -o $RESFILE blender -b $WORKDIR/blender/bmw27_cpu.blend -o $WORKDIR/blenderbmw.png -f 1 --verbose 0 -t 0 &>/dev/null &
 	local PID=$!
 	echo -n -e "* Blender render:\t\t\t"
-	local s='-\|/'; local i=0; while kill -0 $PID &>/dev/null ; do i=$(( (i+1) %4 )); printf "\b${s:$i:1}"; sleep .5; done
+	local s='-+'; local i=0; while kill -0 $PID &>/dev/null ; do i=$(( (i+1) %2 )); printf "\b${s:$i:1}"; sleep 1; done
 	printf "\b " ; cat $RESFILE
 	echo "Blender render: $(cat $RESFILE)" >> $LOGFILE
 	return 0
@@ -45,7 +45,7 @@ runffm() {
 	/usr/bin/time -f %e -o $RESFILE make -s -j${CPUCORES} &>/dev/null &
 	local PID=$!
 	echo -n -e "* ffmpeg compilation:\t\t\t"
-	local s='-\|/'; local i=0; while kill -0 $PID &>/dev/null ; do i=$(( (i+1) %4 )); printf "\b${s:$i:1}"; sleep 1; done
+	local s='-+'; local i=0; while kill -0 $PID &>/dev/null ; do i=$(( (i+1) %2 )); printf "\b${s:$i:1}"; sleep 1; done
 	printf "\b " ; cat $RESFILE
 	echo "ffmpeg compilation: $(cat $RESFILE)" >> $LOGFILE
 	return 0
@@ -54,10 +54,9 @@ runffm() {
 runxz() {
 	local RESFILE="$WORKDIR/runxz"
  	/usr/bin/time -f %e -o $RESFILE xz -z -k -T${CPUCORES} --lzma2=preset=6e,pb=0 -Qqq -f $WORKDIR/firefox60.tar &
-#	/usr/bin/time -f %e -o $RESFILE zstd -k -T${CPUCORES} -19 -qq -f $WORKDIR/kernel49.tar &
 	local PID=$!
 	echo -n -e "* xz compression:\t\t\t"
-	local s='-\|/'; local i=0; while kill -0 $PID &>/dev/null ; do i=$(( (i+1) %4 )); printf "\b${s:$i:1}"; sleep 1; done
+	local s='-+'; local i=0; while kill -0 $PID &>/dev/null ; do i=$(( (i+1) %2 )); printf "\b${s:$i:1}"; sleep 1; done
 	printf "\b " ; cat $RESFILE
 	echo "xz compression: $(cat $RESFILE)" >> $LOGFILE
 	return 0
@@ -68,7 +67,7 @@ runargon() {
 	/usr/bin/time -f %e -o $RESFILE argon2 BenchieSalt -i -t 40 -m 20 -p $CPUCORES &>/dev/null <<< $(dd if=/dev/urandom bs=1 count=64 status=none) &
 	local PID=$!
 	echo -n -e "* argon2 hashing:\t\t\t"
-	local s='-\|/'; local i=0; while kill -0 $PID &>/dev/null ; do i=$(( (i+1) %4 )); printf "\b${s:$i:1}"; sleep 1; done
+	local s='-+'; local i=0; while kill -0 $PID &>/dev/null ; do i=$(( (i+1) %2 )); printf "\b${s:$i:1}"; sleep 1; done
 	printf "\b " ; cat $RESFILE
 	echo "argon2 hashing: $(cat $RESFILE)" >> $LOGFILE
 	return 0
@@ -79,7 +78,7 @@ runperf1() {
 	perf bench -f simple sched messaging -p -t -g 25 -l 10000 1> $RESFILE &
 	local PID=$!
 	echo -n -e "* perf sched:\t\t\t\t"
-	local s='-\|/'; local i=0; while kill -0 $PID &>/dev/null ; do i=$(( (i+1) %4 )); printf "\b${s:$i:1}"; sleep 1; done
+	local s='-+'; local i=0; while kill -0 $PID &>/dev/null ; do i=$(( (i+1) %2 )); printf "\b${s:$i:1}"; sleep 1; done
 	printf "\b " ; cat $RESFILE
 	echo "perf sched: $(cat $RESFILE)" >> $LOGFILE
 	return 0
@@ -90,7 +89,7 @@ runperf2() {
 	/usr/bin/time -f %e -o $RESFILE perf bench -f simple mem memcpy --nr_loops 60 --size 2GB -f x86-64-movsb &>/dev/null &
 	local PID=$!
 	echo -n -e "* perf memcpy:\t\t\t\t"
-	local s='-\|/'; local i=0; while kill -0 $PID &>/dev/null ; do i=$(( (i+1) %4 )); printf "\b${s:$i:1}"; sleep 1; done
+	local s='-+'; local i=0; while kill -0 $PID &>/dev/null ; do i=$(( (i+1) %2 )); printf "\b${s:$i:1}"; sleep 1; done
 	printf "\b " ; cat $RESFILE
 	echo "perf memcpy: $(cat $RESFILE)" >> $LOGFILE
 	return 0
@@ -101,7 +100,7 @@ runpi() {
 	/usr/bin/time -f%e -o $RESFILE $WORKDIR/pi 60000000 1>/dev/null &
 	local PID=$!
 	echo -n -e "* calculating 60m digits of pi:\t\t"
-	local s='-\|/'; local i=0; while kill -0 $PID &>/dev/null ; do i=$(( (i+1) %4 )); printf "\b${s:$i:1}"; sleep 1; done
+	local s='-+'; local i=0; while kill -0 $PID &>/dev/null ; do i=$(( (i+1) %2 )); printf "\b${s:$i:1}"; sleep 1; done
 	printf "\b " ; cat $RESFILE
 	echo "calculating 60m digits of pi: $(cat $RESFILE)" >> $LOGFILE
 	return 0
@@ -114,7 +113,7 @@ killproc() {
 
 exitproc() {
 	echo -e "Removing temporary files...\n"
-	for i in $WORKDIR/{run*,ffmpeg.tar.gz,stress-ng.tar.xz,firefox60.tar.xz,blender*.png,blender,pi.c,stressC,stressR} ; do
+	for i in $WORKDIR/{run*,ffmpeg.tar.gz,stress-ng.tar.xz,firefox60.tar.xz,blender*.png,pi.c,stressC,stressR} ; do
 		[[ -f $i ]] && rm $i
 		[[ -d $i ]] && rm -r $i
 	done
@@ -124,7 +123,7 @@ exitproc() {
 export LANG=C
 CURRDIR=`pwd`
 WORKDIR="$1"
-VER="v1.0"
+VER="v1.1"
 CDATE=`date +%F-%H%M`
 RAMSIZE=`awk '/MemTotal/{print int($2 / 1000)}' /proc/meminfo`
 CPUCORES=`nproc`
@@ -134,6 +133,20 @@ COEFF=$(echo "scale=4; l(${CPUCORES} / 2 + ${CPUFREQ})" | bc -l)
 NRTESTS=9
 SYSINFO=$(inxi -c0 -v | sed "s/Up:.*//;s/inxi:.*//;s/Storage:.*//")
 STRESS=${WORKDIR}/stress-ng/usr/bin/stress-ng
+
+[[ $RAMSIZE -lt 3500 ]] && echo "Your computer must have at least 4 GB of RAM! Aborting." && exit 2
+[[ $CPUCORES -lt 2 ]] && echo "Your CPU must have at least two logical or physical cores! Aborting." && exit 2
+
+[[ -z $1 ]] && echo "Please specify the full path for the temporary directory! Aborting." && exit 4
+
+[[ "${WORKDIR:0:1}" != "/" ]] && WORKDIR="$PWD/$WORKDIR"
+if [[ ! -d "$WORKDIR" ]] ; then
+	read -p "The specified directory $WORKDIR does not exist. Create it (y/N)? " DCHOICE
+	[[ $DCHOICE = "y" || $DCHOICE = "Y" ]] && mkdir -p $WORKDIR || exit 4
+fi
+
+LOGFILE="$WORKDIR/benchie_${CDATE}.log"
+LOCKFILE=`mktemp $WORKDIR/benchie.XXXX`
 
 # stress-ng jobfiles
 cat > $WORKDIR/stressC <<- EOF
@@ -159,14 +172,14 @@ timeout 0
 vm CPUCORES
 vm-method read64
 vm-lock
-vm-bytes 1G
+vm-bytes 1.5G
 EOF
 echo "vm-ops $((12000 / ${CPUCORES}))" >> $WORKDIR/stressR
 cat >> $WORKDIR/stressR <<- EOF
 vm CPUCORES
 vm-method write64
 vm-lock
-vm-bytes 1G
+vm-bytes 1.5G
 EOF
 echo "vm-ops $((12000 / ${CPUCORES}))" >> $WORKDIR/stressR
 cat >> $WORKDIR/stressR <<- EOF
@@ -183,21 +196,7 @@ sed -i "s/CPUCORES/$CPUCORES/g" $WORKDIR/stressR
 #CPUGOV=$(cpupower frequency-info -o | grep -m1 "^CPU" | awk -F' -  ' '{ print $3 }')
 #CPUMHZ=$(lscpu -e=maxmhz | tail -n1)
 #CPUGHZ=$(echo "scale=1; ${CPUMHZ%%,*} / 1000" | bc)
-
-[[ $RAMSIZE -lt 3500 ]] && echo "Your computer must have at least 4 GB of RAM! Aborting." && exit 2
-[[ $CPUCORES -lt 2 ]] && echo "Your CPU must have at least two cores! Aborting." && exit 2
-
-[[ -z $1 ]] && echo "Please specify the full path for the temporary directory! Aborting." && exit 4
-
-[[ "${WORKDIR:0:1}" != "/" ]] && WORKDIR="$PWD/$WORKDIR"
-if [[ ! -d "$WORKDIR" ]] ; then
-	read -p "The specified directory $WORKDIR does not exist. Create it (y/N)? " DCHOICE
-	[[ $DCHOICE = "y" || $DCHOICE = "Y" ]] && mkdir -p $WORKDIR || exit 4
-fi
-
-LOGFILE="$WORKDIR/benchie_${CDATE}.log"
-LOCKFILE=`mktemp $WORKDIR/benchie.XXXX`
-
+echo -e "\nMINI-BENCHMARKER: This script can be slow on older computers, take care!\n"
 read -p "Do you want to drop page cache now? Root priviledges needed! (y/N)" DCHOICE
 [[ $DCHOICE = "y" || $DCHOICE = "Y" ]] && su -c "echo 3 > /proc/sys/vm/drop_caches"
 
@@ -207,12 +206,6 @@ if [[ $CPUGOV != "performance" ]] ; then
 fi
 
 echo -e "\nChecking, downloading and preparing test files...\n"
-
-#if [[ ! -f $WORKDIR/kernel49.tar ]]; then
-#	wget --show-progress -qO $WORKDIR/kernel49.tar.xz https://cdn.kernel.org/pub/linux/kernel/v4.x/linux-4.9.tar.xz
-#	echo "Unzipping kernel tarball..."
-#	xz -d -q $WORKDIR/kernel49.tar.xz
-#fi
 
 if [[ ! -f $WORKDIR/firefox60.tar ]]; then
 	wget --show-progress -qO $WORKDIR/firefox60.tar.xz https://ftp.mozilla.org/pub/firefox/releases/60.9.0esr/source/firefox-60.9.0esr.source.tar.xz
@@ -238,7 +231,7 @@ if [[ ! -d $WORKDIR/stress-ng ]]; then
 	cd .. && rm -rf stress-ng-0.11.14
 fi
 
-if [[ ! -f $WORKDIR/blender.zip ]]; then
+if [[ ! -d $WORKDIR/blender ]]; then
 	wget --show-progress -qO $WORKDIR/blender.zip https://download.blender.org/demo/test/BMW27_2.blend.zip
 	echo "Unzipping Blender demo files..."
     unzip -qqj $WORKDIR/blender.zip -d $WORKDIR/blender
@@ -255,8 +248,8 @@ if [[ ! -d $WORKDIR/ffmpeg-d3b963c ]]; then
   	  --disable-doc --disable-network --disable-protocols --disable-lzma \
   	  --disable-amf --disable-cuda-llvm --disable-cuvid --disable-d3d11va --disable-dxva2 \
   	  --disable-nvdec --disable-nvenc --disable-vaapi --disable-vdpau --disable-sdl2 \
-  	  --disable-schannel --disable-securetransport --enable-libfontconfig \
-  	  --enable-libfreetype --enable-libspeex --enable-libvpx --enable-libopus --enable-libvorbis \
+  	  --disable-schannel --disable-securetransport --disable-libfontconfig \
+  	  --disable-libfreetype --enable-libspeex --enable-libvpx --enable-libopus --enable-libvorbis \
   	  --enable-libx264 --enable-libx265 --enable-opengl --enable-libdrm --enable-gpl \
   	  --enable-gmp --enable-gnutls --disable-avx512 --disable-fma4 --disable-autodetect \
   	  --enable-version3 &>/dev/null
@@ -268,13 +261,13 @@ echo -e "\nStarting...\n" ; sync ; sleep 1
 echo "=====__==__ ========================== _____======"
 echo "====|  \/  |==== MINI BENCHMARKER ====| ___ ))===="
 echo "====| |\/| |======= by torvic9 =======| ___ \====="
-echo "====|_|==|_|========   $VER   ========|_____//===="
+echo "====|_|==|_|=========  $VER  =========|_____//===="
 echo "=================================================="
 
 # start
 trap killproc INT
 trap exitproc EXIT
-runblend ; sleep 3
+
 runstress1; sleep 3
 runstress2; sleep 3
 runperf1 ; sleep 3
@@ -283,21 +276,15 @@ runpi ; sleep 3
 runargon ; sleep 3
 runffm ; sync ; sleep 3
 runxz ; sync ; sleep 3
-
+runblend ; sleep 3
 
 unset arrayz; unset ARRAY
 arrayz=(`awk -F': ' '{print $2}' $LOGFILE`)
-# arrayn not used currently
-# arrayn=(`awk -F': ' '{print $1}' $LOGFILE`)
-# watch!
-#set -x
-# watch!
 
 for ((i=0 ; i<${NRTESTS} ; i++)) ; do
 	ARRAY[$i]="$(echo "scale=10; sqrt(${arrayz[$i]} * $COEFF * 100)" | bc -l)"
 done
 
-#TTIME="$(echo "${arrayz[@]}" | sed 's/ /+/g' | bc)"
 TTIME="$(IFS="+" ; bc <<< "scale=2; ${arrayz[*]}")"
 INTSCORE="$(IFS="+" ; bc -l <<< "scale=2; ${ARRAY[*]}")"
 SCORE="$(bc -l <<< "scale=2; $INTSCORE / $NRTESTS")"
@@ -306,7 +293,6 @@ echo "Total time in seconds:"
 echo "--------------------------------------------------"
 echo $TTIME ; echo "Total time (s): $TTIME" >> $LOGFILE
 echo "--------------------------------------------------"
-#echo "Total score (lower is better):"
 echo -n "Total score (lower is better)" ; echo " [multi = $COEFF]:"
 echo "--------------------------------------------------"
 echo $SCORE ; echo "Total score: $SCORE" >> $LOGFILE
